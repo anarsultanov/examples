da1966f59350004c97cff861f484fa43688c3a7a
Anar Sultanov <anar.sultanov@assessio.se>
2019-06-11 20:07:28 +0200

    Create modified token granter for "password" grant type

---
+++ spring-boot-oauth2-mfa/src/main/java/dev/sultanov/springboot/oauth2/mfa/config/granter/PasswordTokenGranter.java
@@ -0,0 +1,62 @@
+package dev.sultanov.springboot.oauth2.mfa.config.granter;
+
+import dev.sultanov.springboot.oauth2.mfa.exception.MfaRequiredException;
+import dev.sultanov.springboot.oauth2.mfa.service.MfaService;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.InvalidGrantException;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
+import org.springframework.security.oauth2.provider.TokenRequest;
+import org.springframework.security.oauth2.provider.token.AbstractTokenGranter;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class PasswordTokenGranter extends AbstractTokenGranter {
+    private static final String GRANT_TYPE = "password";
+    private static final GrantedAuthority PRE_AUTH = new SimpleGrantedAuthority("PRE_AUTH");
+
+    private final AuthenticationManager authenticationManager;
+    private final MfaService mfaService;
+
+    public PasswordTokenGranter(AuthorizationServerEndpointsConfigurer endpointsConfigurer, AuthenticationManager authenticationManager, MfaService mfaService) {
+        super(endpointsConfigurer.getTokenServices(), endpointsConfigurer.getClientDetailsService(), endpointsConfigurer.getOAuth2RequestFactory(), GRANT_TYPE);
+        this.authenticationManager = authenticationManager;
+        this.mfaService = mfaService;
+    }
+
+    @Override
+    protected OAuth2Authentication getOAuth2Authentication(ClientDetails client, TokenRequest tokenRequest) {
+        Map<String, String> parameters = new LinkedHashMap<>(tokenRequest.getRequestParameters());
+        String username = parameters.get("username");
+        String password = parameters.get("password");
+        parameters.remove("password");
+        Authentication userAuth = new UsernamePasswordAuthenticationToken(username, password);
+        ((AbstractAuthenticationToken) userAuth).setDetails(parameters);
+
+        try {
+            userAuth = this.authenticationManager.authenticate(userAuth);
+        } catch (AccountStatusException | BadCredentialsException e) {
+            throw new InvalidGrantException(e.getMessage());
+        }
+
+        if (userAuth != null && userAuth.isAuthenticated()) {
+            OAuth2Request storedOAuth2Request = this.getRequestFactory().createOAuth2Request(client, tokenRequest);
+            if (mfaService.isEnabled(username)) {
+                userAuth = new UsernamePasswordAuthenticationToken(username, password, Collections.singleton(PRE_AUTH));
+                OAuth2AccessToken accessToken = getTokenServices().createAccessToken(new OAuth2Authentication(storedOAuth2Request, userAuth));
+                throw new MfaRequiredException(accessToken.getValue());
+            }
+            return new OAuth2Authentication(storedOAuth2Request, userAuth);
+        } else {
+            throw new InvalidGrantException("Could not authenticate user: " + username);
+        }
+    }
+}
