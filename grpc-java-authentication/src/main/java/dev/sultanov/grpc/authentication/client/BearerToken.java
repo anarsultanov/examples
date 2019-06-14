b1252a8756e1b9a761b6fb55858c50b0f6e3caed
Anar Sultanov <anar.sultanov@assessio.se>
2019-06-14 17:00:51 +0200

    Include call credentials

---
+++ grpc-java-authentication/src/main/java/dev/sultanov/grpc/authentication/client/BearerToken.java
@@ -0,0 +1,35 @@
+package dev.sultanov.grpc.authentication.client;
+
+import dev.sultanov.grpc.authentication.shared.Constants;
+import io.grpc.CallCredentials;
+import io.grpc.Metadata;
+import io.grpc.Status;
+
+import java.util.concurrent.Executor;
+
+public class BearerToken extends CallCredentials {
+
+    private String value;
+
+    BearerToken(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public void applyRequestMetadata(RequestInfo requestInfo, Executor executor, MetadataApplier metadataApplier) {
+        executor.execute(() -> {
+            try {
+                Metadata headers = new Metadata();
+                headers.put(Constants.AUTHORIZATION_METADATA_KEY, String.format("%s %s", Constants.BEARER_TYPE, value));
+                metadataApplier.apply(headers);
+            } catch (Throwable e) {
+                metadataApplier.fail(Status.UNAUTHENTICATED.withCause(e));
+            }
+        });
+    }
+
+    @Override
+    public void thisUsesUnstableApi() {
+        // noop
+    }
+}
--- grpc-java-authentication/src/main/java/dev/sultanov/grpc/authentication/client/GreetingClient.java
+++ grpc-java-authentication/src/main/java/dev/sultanov/grpc/authentication/client/GreetingClient.java
@@ -1,9 +1,12 @@
 package dev.sultanov.grpc.authentication.client;
 
+import dev.sultanov.grpc.authentication.shared.Constants;
 import dev.sultanov.grpc.authentication.shared.GreetingRequest;
 import dev.sultanov.grpc.authentication.shared.GreetingResponse;
 import dev.sultanov.grpc.authentication.shared.GreetingServiceGrpc;
 import io.grpc.ManagedChannelBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 public class GreetingClient {
 
@@ -11,9 +14,19 @@ public class GreetingClient {
         var channel = ManagedChannelBuilder.forAddress("localhost", 8080)
                 .usePlaintext()
                 .build();
-        var stub = GreetingServiceGrpc.newBlockingStub(channel);
+        BearerToken token = new BearerToken(getJwt());
+        var stub = GreetingServiceGrpc.newBlockingStub(channel)
+                .withCallCredentials(token);
+
         GreetingRequest request = GreetingRequest.newBuilder().setName("John").build();
         GreetingResponse response = stub.greeting(request);
         System.out.println(response.getGreeting());
+    }
+
+    private static String getJwt() {
+        return Jwts.builder()
+                .setSubject("GreetingClient")
+                .signWith(SignatureAlgorithm.HS256, Constants.JWT_SIGNING_KEY)
+                .compact();
     }
 }
