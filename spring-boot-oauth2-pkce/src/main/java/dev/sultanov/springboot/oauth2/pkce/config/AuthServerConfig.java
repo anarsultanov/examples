c0a7d59601f7c7d71d04689549d05857df305010
Anar Sultanov <anar.sultanov@assessio.se>
2020-01-13 10:27:52 +0100

    spring-boot-oauth2-pkce: Configure authorization server

---
+++ spring-boot-oauth2-pkce/src/main/java/dev/sultanov/springboot/oauth2/pkce/config/AuthServerConfig.java
@@ -0,0 +1,28 @@
+package dev.sultanov.springboot.oauth2.pkce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) {
+        security.allowFormAuthenticationForClients();
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("client")
+                .secret("{noop}")
+                .redirectUris("http://localhost:8080/")
+                .authorizedGrantTypes("authorization_code")
+                .scopes("read")
+                .autoApprove(true);
+    }
+}
---
+++ spring-boot-oauth2-pkce/src/main/java/dev/sultanov/springboot/oauth2/pkce/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package dev.sultanov.springboot.oauth2.pkce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .requestMatchers().antMatchers("/login", "/oauth/authorize")
+                .and()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("john")
+                .password(passwordEncoder().encode("pass"))
+                .roles("USER");
+    }
+}
