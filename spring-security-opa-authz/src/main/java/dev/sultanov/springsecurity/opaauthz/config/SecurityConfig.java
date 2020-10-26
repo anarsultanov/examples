0a47ae3a04f7b682cfb49b59a8bd07d2ff55b75e
Anar Sultanov <anar.sultanov@assessio.se>
2020-10-26 10:20:19 +0100

    spring-security-opa-authz: Enable global method security

--- spring-security-opa-authz/src/main/java/dev/sultanov/springsecurity/opaauthz/config/SecurityConfig.java
+++ spring-security-opa-authz/src/main/java/dev/sultanov/springsecurity/opaauthz/config/SecurityConfig.java
@@ -3,12 +3,14 @@ package dev.sultanov.springsecurity.opaauthz.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
