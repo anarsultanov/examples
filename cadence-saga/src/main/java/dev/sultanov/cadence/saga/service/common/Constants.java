5c558b73afe595db7818eae55a6aa781133ca9e4
Anar Sultanov <anar.sultanov@assessio.se>
2021-05-04 11:37:26 +0200

    cadence-saga: Add constants

---
+++ cadence-saga/src/main/java/dev/sultanov/cadence/saga/service/common/Constants.java
@@ -0,0 +1,7 @@
+package dev.sultanov.cadence.saga.service.common;
+
+public class Constants {
+    public static final String DOMAIN = "example";
+    public static final String CUSTOMER_TASK_LIST = "CustomerTaskList";
+    public static final String ORDER_TASK_LIST = "OrderTaskList";
+}
