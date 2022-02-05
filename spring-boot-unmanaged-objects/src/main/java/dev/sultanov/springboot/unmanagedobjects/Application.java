433c16dc2a7afb316011fba935adb96ad1da82e6
Anar Sultanov <anar.sultanov@assessio.se>
2022-02-05 19:23:24 +0100

    spring-boot-unmanaged-objects: Initialize module

---
+++ spring-boot-unmanaged-objects/src/main/java/dev/sultanov/springboot/unmanagedobjects/Application.java
@@ -0,0 +1,12 @@
+package dev.sultanov.springboot.unmanagedobjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
