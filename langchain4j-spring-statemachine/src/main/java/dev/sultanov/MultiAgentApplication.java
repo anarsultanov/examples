42d76b39f5a788c66dff31ea14d65f7a563bf773
Anar Sultanov <anar.sultanov@assessio.se>
2024-09-02 16:04:11 +0200

    langchain4j-spring-statemachine: Setup project

---
+++ langchain4j-spring-statemachine/src/main/java/dev/sultanov/MultiAgentApplication.java
@@ -0,0 +1,13 @@
+package dev.sultanov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.
+                run(MultiAgentApplication.class, args);
+    }
+}
